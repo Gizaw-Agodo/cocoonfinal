b425b42a6835fc601b7c06432abdff322a8696ff
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-05-12 22:07:02 +0300

    front-end

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signUp, logIn } = require("../controllers/auth");
-
-router.post("/logIn", logIn);
-router.post("/signUp", signUp);
-
-
-module.exports = router;
